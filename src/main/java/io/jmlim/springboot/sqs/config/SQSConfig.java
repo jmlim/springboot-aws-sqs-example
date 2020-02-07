736900a1f723e22c7b744f352b9a9f9b693e5a88
Lim Jeong Muk <jmlim@neowiz.com>
2020-02-07 11:36:49 +0900

    listener 와 producer sqs객체 같은 것을 사용.

--- src/main/java/io/jmlim/springboot/sqs/config/SQSConfig.java
+++ src/main/java/io/jmlim/springboot/sqs/config/SQSConfig.java
@@ -6,6 +6,7 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,23 +14,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SQSConfig {
 
-	@Value("${cloud.aws.region.static}")
-	private String region;
+    @Value("${cloud.aws.region.static}")
+    private String region;
 
-	@Value("${cloud.aws.credentials.access-key}")
-	private String awsAccessKey;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String awsAccessKey;
 
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String awsSecretKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String awsSecretKey;
 
-	@Bean
-	public QueueMessagingTemplate queueMessagingTemplate() {
-		return new QueueMessagingTemplate(amazonSQSAsync());
-	}
+    /**
+     * https://stackoverflow.com/questions/40092518/how-to-configure-custom-spring-cloud-aws-simplemessagelistenercontainerfactory-s
+     *
+     * @return
+     */
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory() {
+        SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
+        factory.setAmazonSqs(amazonSQSAsync());
+        factory.setMaxNumberOfMessages(10);
+        factory.setWaitTimeOut(20);// Long polling 설정
+        return factory;
+    }
 
-	public AmazonSQSAsync amazonSQSAsync() {
-		return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.fromName(region))
-				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
-				.build();
-	}
+    @Bean
+    public QueueMessagingTemplate queueMessagingTemplate() {
+        return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    public AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                .build();
+    }
 }
